68ec14121bc72a1322fc39b8de4629372bbc59da
ROK1SMR <runnerbkln@gmail.com>
2021-07-25 13:02:29 +0300

    First commit of the cats app status 25.07.21

---
+++ Cat Pics App/NetworkManager.swift
@@ -0,0 +1,93 @@
+//
+//  NetworkManager.swift
+//  Cat Pics App
+//
+//  Created by Konstantin on 21.02.2021.
+//
+
+import Foundation
+import UIKit
+
+
+//struct LikedLinks{
+//    let id: String
+//    let url: String
+//
+//    init(id: String, url: String){
+//        self.id = id
+//        self.url = url
+//    }
+//}
+
+
+struct ApiResponseDataModel: Codable {
+    var id: String
+    var url: String
+  
+}
+
+class NetworkManager {
+    static var shared = NetworkManager() // патерн singleton, если не понятно поясню
+    init() { }
+    
+    func getImageURL(handler: @escaping (URL?) -> () ) {
+        let urlString = "https://api.thecatapi.com/v1/images/search"
+        guard let url = URL(string: urlString) else {
+            DispatchQueue.main.async {
+                handler(nil)
+            }
+            return
+        }
+        
+        
+        DispatchQueue.global().async {
+            let session = URLSession.shared
+            let task = session.dataTask(with: url) { (data, _, err) in
+                if let err = err {
+                    print(err)
+                    DispatchQueue.main.async {
+                        handler(nil)
+                    }
+                } else if let data = data {
+                    if let apiModel = try? JSONDecoder().decode([ApiResponseDataModel].self, from: data),
+                       let item = apiModel.first,
+                       let url = URL(string: item.url)
+                    {
+                        DispatchQueue.main.async {
+                            print(item)
+                            handler(url)
+                        }
+                    } else {
+                        DispatchQueue.main.async {
+                            handler(nil)
+                        }
+                    }
+                }
+            }
+            task.resume()
+        }
+    } 
+    
+    
+    func getImageFromURL(_ url: URL, handler: @escaping (UIImage?) -> ()) {
+        DispatchQueue.main.async {
+            let session = URLSession.shared
+            let task = session.dataTask(with: url) { (data, _, err) in
+                if let err = err {
+                    DispatchQueue.main.async {
+                        handler(nil)
+                    }
+                } else if let data = data, let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        handler(image)
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        handler(nil)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+}
--- Cat Pics App/ViewController.swift
+++ Cat Pics App/ViewController.swift
@@ -5,15 +5,105 @@
 //  Created by Konstantin on 15.11.2020.
 //
 
+// colors used: https://colorhunt.co/palette/220248
+
 import UIKit
 
 class ViewController: UIViewController {
+    
+////    creating the user defaults
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadImage()
+        
+        let defaults = UserDefaults.standard
+        defaults.set(likedArray, forKey: "SavedArray")
+        let savedArray = defaults.object(forKey: "SavedArray") as? [String] ?? [String]()
+        print(savedArray)
+    }
+    
+    
+    struct theCatAPI: Decodable {
+        var url: String!
+    }
+    var cats = theCatAPI()
+    
+    var likedArray:[URL] = []
+    var currentURL: URL?
+    
+    @IBOutlet weak var catsHere: UIImageView!
+    @IBAction func buttonNext(_ sender: UIButton) {
+        loadImage()
+    }
+    
+    
+    @IBAction func buttonLike(_ sender: UIButton) {
+        guard let currentURL = currentURL else { return }
+        
+        likedArray.append(currentURL)
+        print(likedArray)
+    }
+    
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+    
+    func loadImage() {
+        setProgressIndicatorEnabled(true)
+        NetworkManager.shared.getImageURL { [weak self] (url) in
+            
+            guard let url = url else {
+                self?.showProblemAlert()
+                self?.setProgressIndicatorEnabled(false)
+                return
+            }
+            
+            // получили нашу ссылку для фото
+            
+            NetworkManager.shared.getImageFromURL(url) { (image) in
+                defer {
+                    self?.setProgressIndicatorEnabled(false)
+                }
+                
+                guard let image = image else {
+                    self?.catsHere.image = nil
+                    self?.currentURL = nil
+                    self?.showProblemAlert()
+                    return
+                }
+                
+                self?.catsHere.image = image
+                self?.currentURL = url
+            }
+        }
     }
 
-
+    
+// протянул из сториборда элемент UIActivityIndicatorView и добавил его название в функцию, по умолчанию он не работает
+    func setProgressIndicatorEnabled(_ enabled: Bool) {
+        loadingIndicator.isHidden = !enabled
+        if enabled {
+            loadingIndicator.startAnimating()
+        } else {
+            loadingIndicator.stopAnimating()
+        }
+    }
+    
+    private var progressIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView()
+        indicator.translatesAutoresizingMaskIntoConstraints = false
+        indicator.backgroundColor = .white
+        return indicator
+        
+    }()
+    
+    private func showProblemAlert() {
+        let alertController = UIAlertController(title: "Some problem", message: "", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+        
 }
+
+
 
